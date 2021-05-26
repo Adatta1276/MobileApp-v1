b2e606a46fc224003409f25fa8fde56e9d5ac404
Adatta1276 <73747859+Adatta1276@users.noreply.github.com>
2021-05-26 18:43:04 +0530

    Delete AppInfo.js

--- AppInfo.js
+++
@@ -1,9 +0,0 @@
-var AllData = {
-    databaseName : CompassData,
-    databaseNodeCount : 6,
-    nodesWithChildren : 5,
-    nodesWithoutChildren : 1,
-    nodes : [CompassFaceData,CompassValues,GeoData,HeadingToLocation,LocationEnabled,ScreenData],
-    nodeWithoutChildren : LocationEnabled,
-    
-}
